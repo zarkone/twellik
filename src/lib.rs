88c31b41820af6061b5ca9a1f5531eb3913f5d6b
Anatolii Smolianinov <zarkonesmall@gmail.com>
2023-10-24 01:50:28 +0200

    add "create collection"

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,23 @@ extern "C" {
 
     #[wasm_bindgen(js_namespace = console, js_name = log)]
     fn log(s: &str);
+
+    #[wasm_bindgen(js_namespace = localStorage, js_name = setItem)]
+    fn local_storage_set_item(key: &str, val: &str);
+}
+
+fn twellik_log(s: &str) {
+    log(format!("Twellik: {s}").as_str())
+}
+fn make_local_storage_collection_name(name: &str) -> String {
+    let prefix = "__twellik";
+    format!("{prefix}_{name}")
+}
+#[wasm_bindgen]
+pub fn create_collection(name: &str) {
+    let local_storage_name = make_local_storage_collection_name(&name);
+    local_storage_set_item(&local_storage_name, &"");
+    twellik_log(format!("{local_storage_name} collection created.").as_str())
 }
 
 #[wasm_bindgen]
