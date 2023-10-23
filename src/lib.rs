e96c984c9c445736372d2fd2a8b050523362c3f9
Anatolii Smolianinov <zarkonesmall@gmail.com>
2023-10-24 01:20:43 +0200

    use console.log instead of alert

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,12 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern "C" {
     pub fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    log(&format!("Hello, {}!", name));
 }
