2979fbad3ff5b975b35b5be0107d231df7f2ffe8
Anatolii Smolianinov <zarkonesmall@gmail.com>
2023-10-29 11:39:44 +0100

    upsert

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 
 use serde::{Deserialize, Serialize};
+use serde_json;
 use serde_wasm_bindgen;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -36,10 +37,12 @@ fn make_local_storage_collection_name(name: &str) -> String {
 }
 
 #[wasm_bindgen]
-pub fn create_collection(name: &str) {
+pub fn create_collection(name: &str) -> Result<(), JsValue> {
     let local_storage_name = make_local_storage_collection_name(&name);
+
     local_storage_set_item(&local_storage_name, &"");
-    twellik_log(format!("{local_storage_name} collection created.").as_str())
+    twellik_log(format!("{local_storage_name} collection created.").as_str());
+    Ok(())
 }
 
 // TODO: how to pass types from/to JS land:
@@ -47,8 +50,14 @@ pub fn create_collection(name: &str) {
 
 #[wasm_bindgen]
 pub fn upsert_points(coll_name: &str, points: JsValue) -> Result<(), JsValue> {
-    let p: Vec<Point> = serde_wasm_bindgen::from_value(points.clone())?;
-    twellik_log(format!("points: {:?}", &p).as_str());
+    // TODO: probably can just passthrough
+    let rs_points: Vec<Point> = serde_wasm_bindgen::from_value(points.clone())?;
+    let js_points = match serde_json::to_string(&rs_points) {
+        Ok(p) => p,
+        Err(e) => e.to_string(),
+    };
+    twellik_log(format!("uprerting points in {coll_name}: {:?}", &rs_points).as_str());
+    local_storage_set_item(coll_name, &js_points);
     Ok(())
 }
 
