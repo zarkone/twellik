d1b60c11d36f9458d6f5e5b658804e6ab9449f7b
Anatolii Smolianinov <zarkonesmall@gmail.com>
2023-10-29 11:03:21 +0100

    add a sample; wip

--- src/lib.rs
+++ src/lib.rs
@@ -40,12 +40,25 @@ pub fn create_collection(name: &str) {
 
 // TODO: how to pass types from/to JS land:
 // https://rustwasm.github.io/wasm-bindgen/reference/arbitrary-data-with-serde.html?highlight=array#javascript-usage
+
 #[wasm_bindgen]
-pub fn upsert_points(points: JsValue) {}
+pub fn upsert_points(coll_name: &str, points: JsValue) {
+    twellik_log(format!("points: {:?}", points).as_str());
+}
 
 /// Reads collection into memory.
 fn read_collection(name: &str) -> Collection {
     todo!()
 }
+
 #[wasm_bindgen]
 pub fn scroll_points(query: &str) {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 3;
+        assert_eq!(result, 4);
+    }
+}
