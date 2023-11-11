c5dccca119d7da912ff7d7cde0c724431af9d17a
Anatolii Smolianinov <zarkonesmall@gmail.com>
2023-11-11 01:49:01 +0100

    rename to similarity

--- src/cosine.rs
+++ src/cosine.rs
@@ -6,7 +6,7 @@ fn dot_product(a: &Vec<f64>, b: &Vec<f64>) -> f64 {
     a.iter().zip(b.iter()).fold(0.0, dot_product_acc)
 }
 
-pub fn distance(a: &Vec<f64>, b: &Vec<f64>) -> f64 {
+pub fn similarity(a: &Vec<f64>, b: &Vec<f64>) -> f64 {
     assert!(
         a.len() == b.len(),
         "cosine distance: vectors must have the same lenght"
@@ -18,6 +18,10 @@ pub fn distance(a: &Vec<f64>, b: &Vec<f64>) -> f64 {
     let norm_b = dot_product(&b, &b).sqrt();
 
     product / (norm_a * norm_b)
+}
+
+pub fn distance(a: &Vec<f64>, b: &Vec<f64>) -> f64 {
+    1.0 - similarity(a, b)
 }
 
 #[cfg(test)]
@@ -44,17 +48,17 @@ mod tests {
             [1.3, 1.3, 0.0],
         ];
 
-        let r1 = distance(&v[14].to_vec(), &v[0].to_vec());
+        let r1 = similarity(&v[14].to_vec(), &v[0].to_vec());
         assert_eq!(r1, 0.993472672904106);
 
-        let r2 = distance(&v[14].to_vec(), &v[1].to_vec());
+        let r2 = similarity(&v[14].to_vec(), &v[1].to_vec());
         assert_eq!(r2, 0.9896970521184516);
 
-        let r3 = distance(&v[14].to_vec(), &v[4].to_vec());
+        let r3 = similarity(&v[14].to_vec(), &v[4].to_vec());
         assert_eq!(r3, 0.9438583563660174);
 
         let v2 = [[6.6, 6.2], [9.7, 9.9]];
-        let r4 = distance(&v2[0].to_vec(), &v2[1].to_vec());
+        let r4 = similarity(&v2[0].to_vec(), &v2[1].to_vec());
         assert_eq!(r4, 0.9991413385403556);
     }
 }
